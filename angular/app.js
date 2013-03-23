a845a3a503d183a2227ad9f4085d0c40ed1939ca
Imbolc <imbolc@imbolc.name>
2013-03-23 15:39:11 +0700

    remove multifiles angular example, update one-file example

--- angular/app.js
+++ angular/app.js
@@ -1,17 +1,56 @@
 /*jslint node: true */
-/*global angular, FilmListCtrl, FilmDetailCtrl */
+/*global angular */
 'use strict';
 
-angular.module('filmList', ['filmFilters', 'filmServices'])
-    .config(['$routeProvider', function ($routeProvider) {
+//////////////
+// Controllers
+// ///////////
+
+var app = angular.module('filmList', ['ngResource']);
+
+// Routing
+app.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        //$locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix = '!';
         $routeProvider
             .when('/films', {
                 templateUrl: 'film-list.html',
-                controller: FilmListCtrl
+                controller: 'FilmListCtrl'
             })
             .when('/films/:filmId', {
                 templateUrl: 'film-detail.html',
-                controller: FilmDetailCtrl
+                controller: 'FilmDetailCtrl'
             })
             .otherwise({redirectTo: '/films'});
     }]);
+
+// Services
+app.factory('Film', function ($resource) {
+    return $resource('films.json', {}, {
+        query: {method: 'GET', params: {}, isArray: true}
+    });
+});
+
+// Controllers
+app.controller('FilmListCtrl', ['$scope', 'Film', function ($scope, Film) {
+    $scope.films = Film.query();
+    $scope.orderProp = '-rating';
+
+    $scope.incRating = function (film) {
+        film.rating += 1;
+    };
+}]);
+
+app.controller('FilmDetailCtrl', ['$scope', '$routeParams',
+    function ($scope, $routeParams) {
+        $scope.filmId = $routeParams.filmId;
+    }]);
+
+
+// Filters
+app.filter('starRating', function () {
+    return function (n) {
+        return (new Array(+n)).join('*');
+    };
+});
